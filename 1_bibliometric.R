ae1fe0b6807c3c537ee06d80a597014a2a34aeec
Carlos Carbajal <carbajal.carlos@hotmail.com>
2018-05-07 16:18:02 +0100

    - improve readability - start of the references analysis

--- 1_bibliometric.R
+++ 1_bibliometric.R
@@ -1,12 +1,13 @@
 # Installation and loading of required libraries
 require("bibliometrix")
-#library(bibliometrix) # this line might not be necessary after the previous one
 
 # Importing data from WoS-export
 bm <- readFiles("0_data/wos-digital_innovation-2000-2018.bib")
 bm <- convert2df(bm, dbsource = "isi", format = "bibtex")
+
 # Blibliometric analysis
 bm_analysis <- biblioAnalysis(bm, sep = ";")
 bm_summary <- summary(object = bm_analysis, k = 10, pause = FALSE)
-plot(x = bm_analysis, k = 5, pause = FALSE)
+plot(x = bm_analysis, k = 10, pause = TRUE)
 
+# References analysis
