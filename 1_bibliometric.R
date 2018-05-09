d0cb830ccad539b45527341e301e26569f4ee334
Carlos Carbajal <carbajal.carlos@hotmail.com>
2018-05-09 09:44:31 +0100

    small advancement, pausing for markdown training.

--- 1_bibliometric.R
+++ 1_bibliometric.R
@@ -1,9 +1,13 @@
+# ----- Initialization of the project -----
 # Installation and loading of required libraries
 require("bibliometrix")
 
 # Importing data from WoS-export
 bm <- readFiles("0_data/wos-digital_innovation-2000-2018.bib")
 bm <- convert2df(bm, dbsource = "isi", format = "bibtex")
+
+# ----- Data cleaning -----
+
 
 # Blibliometric analysis
 bm_analysis <- biblioAnalysis(bm, sep = ";")
