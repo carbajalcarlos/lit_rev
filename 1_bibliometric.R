27014ddf39b0bdac55dd1764aec7dad7b8581513
Carlos Carbajal <carbajal.carlos@hotmail.com>
2018-05-11 15:19:33 +0100

    Start of the cleaning process

--- 1_bibliometric.R
+++ 1_bibliometric.R
@@ -7,6 +7,12 @@ bm <- readFiles("0_data/wos-digital_innovation-2000-2018.bib")
 bm <- convert2df(bm, dbsource = "isi", format = "bibtex")
 
 # ----- Data cleaning -----
+for (i in 1:length(bm)) {
+  if (class(bm[ ,i]) == "character") {
+    tolower(bm[ ,i])
+  }
+}
+
 
 
 # Blibliometric analysis
