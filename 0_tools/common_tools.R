ba9859d42f91f1496f309fac627b9308f98b2e0e
Carlos Carbajal <carbajal.carlos@hotmail.com>
2018-05-29 14:43:26 +0100

    Organizing tools used in the cleaning process.

--- 0_tools/common_tools.R
+++ 0_tools/common_tools.R
@@ -12,10 +12,24 @@ i <- index[1]
 
 index <- grep(pattern = "^[[:punct:]].*", x = bg_df$title); bg_df$title[index]
 
-pat <- gsub(pattern = "[^[:punct:]]", replacement = "", x = bg_df$title)
+# Lookout of specific characters
+index <- grep(pattern = ";", x = bg_df$abstract, fixed = TRUE)
+temp <- bg_df$abstract[index]; temp
+
+# List of punctuation signs in an object
+pat <- bg_df$abstract
+pat <- gsub(pattern = "[^[:punct:]]", replacement = "", x = pat)
 pat <- trimws(unlist(strsplit(pat, split = "")), which = "both")
 pat <- as.data.frame(x = table(pat), stringsAsFactors = FALSE)
 
-index <- grep(pattern = "?", x = bg_df$title, fixed = TRUE)
-temp <- bg_df$title[index]; temp
+# List of words used in an object
+wordlist <- bg_df$abstract
+wordlist <- trimws(unlist(strsplit(wordlist, split = " ")), which = "both")
+wordlist <- as.data.frame(x = table(wordlist), stringsAsFactors = FALSE)
 
+# Extracting patterns in an object
+pat <- bg_df$unique.id
+pat <- gsub(pattern = "[[:alpha:]]", replacement = "A", x = pat)
+pat <- gsub(pattern = "[[:digit:]]", replacement = "D", x = pat)
+pat <- trimws(unlist(strsplit(pat, split = "")), which = "both")
+pat <- as.data.frame(x = table(pat), stringsAsFactors = FALSE)
